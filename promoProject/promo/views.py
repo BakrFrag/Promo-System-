4f88d5e06b44b9e6e40f16422ab12a81dd0e7fa9
bakrfrag <bakr.frag@gmail.com>
2020-12-20 16:27:27 +0200

    define promo viewset and set get_serializer_class dynmically

--- promoProject/promo/views.py
+++ promoProject/promo/views.py
@@ -8,3 +8,17 @@ from .serializers import *;
 from rest_framework.permissions import IsAdminUser,IsAuthenticated;
 from rest_framework.response import Response;
 from rest_framework.validators import ValidationError;
+class PromoViewSet(ModelViewSet):
+    '''
+    '''
+    def get_serializer_class(self):
+        '''
+        handle serializer dynamically acording to request action
+        '''
+        action=self.action;
+        if action=='list' or action=='retrieve':
+            return promoListSerializer;
+        elif action=='create'or action=='update':
+            return promoSerializer
+        elif action=='partial_update':
+            return promoPartialSerializer;

cdc4b9c06ea52af18ad9c72e7d7ec5286ac8a659
bakrfrag <bakr.frag@gmail.com>
2020-12-20 16:00:22 +0200

    add custom manager to handle admin users

--- promoProject/promo/models.py
+++ promoProject/promo/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User;
+class AdminManager(models.Manager):
+    def get_queryset(self):
+         return super(AdminManager,self).get_queryset().filter(is_superuser=True);

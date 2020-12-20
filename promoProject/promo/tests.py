9609e3a0363516ce0359807938404beb54ce293c
bakrfrag <bakr.frag@gmail.com>
2020-12-20 20:13:41 +0200

    add tests with normal user

--- promoProject/promo/tests.py
+++ promoProject/promo/tests.py
@@ -18,19 +18,34 @@ class TestPromoViewset(unittest.TestCase):
         response=self.client.delete(reverse('delete_promo',kwargs={"pk":100}));
         print(response.status_code)
         self.assertEqual(response.status_code,404);
-    # def test_update(self):
-    #     response=self.client.put(reverse('update_promo',kwargs={
-    #         "start":"2020-12-24T20:16:00Z",
-    #         "end":"2020-12-31T20:16:00Z",
-    #         "kind":"points","user":3
-    #     }));
-    #     print(response.status_code)
-    #     self.assertEqual(response.status_code,200);
     def test_partial_update(self):
        
         response=self.client.patch(reverse('promo_partial_update',kwargs={
             "pk":5
         }),{"amount":1000});
-        self.assertEqual(response.status_code,403)
+        self.assertEqual(response.status_code,403);
+class testPromoNormalUser(unittest.TestCase):
+    def setUp(self):
+    
+        self.client=APIClient();
+        self.client.login(username='normal1',password='normal147258369');
+    def test_list(self):
+        # Test retrieve list of objects 
+        response=self.client.get(reverse('promo_list'));
+        self.assertEqual(response.status_code,200);
+        # Test retrieve specfic object
+        response=self.client.get(reverse('promo_list'),kwargs={'pk':7,'q':'remaining'});
+        print(response.status_code)
+        self.assertEqual(response.status_code,200);
+    def test_delete(self):
+        response=self.client.delete(reverse('delete_promo',kwargs={"pk":100}));
+        print(response.status_code)
+        self.assertEqual(response.status_code,403);
+    def test_partial_update(self):
+       
+        response=self.client.patch(reverse('promo_partial_update',kwargs={
+            "pk":100
+        }),{"amount":1000});
+        self.assertEqual(response.status_code,404);
 
 

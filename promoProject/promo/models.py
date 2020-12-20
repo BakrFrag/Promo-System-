fb47cd656b9e55fe726f845b17d98ff56039f2e9
bakrfrag <bakr.frag@gmail.com>
2020-12-20 16:01:36 +0200

    add custom manager to handle normal users

--- promoProject/promo/models.py
+++ promoProject/promo/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User;
 class AdminManager(models.Manager):
+    '''
+    custom manager to handle superusers
+    '''
     def get_queryset(self):
          return super(AdminManager,self).get_queryset().filter(is_superuser=True);
+class NormalManager(models.Manager):
+    '''
+    custom manager to handle normal users
+    '''
+    def get_queryset(self):
+          return super(NormalManager,self).get_queryset().filter(is_superuser=False);
